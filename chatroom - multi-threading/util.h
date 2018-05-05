cf45ccc38bfd4b9d94190ccc37507c779b8cecb4
Daniel Suissa <suissa.daniel@gmail.com>
2018-05-05 15:56:38 -0400

    cancel debugging

--- chatroom - multi-threading/util.h
+++ chatroom - multi-threading/util.h
@@ -4,7 +4,7 @@
 #define MAX_NAME_LENGTH 5
 #define MAX_TOTAL_LENGTH MAX_MESSAGE_LENGTH + MAX_NAME_LENGTH + 2 //includes a colon and a white space
 #define DEFAULT_PORT 5197
-#define DEBUGON 1
+#define DEBUGON 0
 #define DEBUG if(DEBUGON)
 
 void error_out(char *message);
